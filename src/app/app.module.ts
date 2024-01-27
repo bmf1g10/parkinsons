7ae7e6651a6b39714652c7314f0593bab5ba7a76
Ben Fox <bmf1g10@gmail.com>
2024-01-27 18:15:40 +0000

    call octopus data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/material/material.module';
 import { ComponentsModule } from './components/components.module';
 import { ContainersModule } from './containers/containers.module';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -17,7 +19,8 @@ import { ContainersModule } from './containers/containers.module';
     BrowserAnimationsModule,
     MaterialModule,
     ComponentsModule,
-    ContainersModule
+    ContainersModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/containers/home/home.component.ts
+++ src/app/containers/home/home.component.ts
@@ -1,10 +1,107 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Chart, LinearScale,  BarController, CategoryScale, BarElement } from 'chart.js';
+
+Chart.register(LinearScale, BarController, CategoryScale, BarElement);
+
+interface ResultsItem {
+  interval_start: string; 
+  interval_end: string; 
+  consumption: number;
+}
+interface DataItem {
+  count: number;
+  next: string;
+  previous: string;
+  results: ResultsItem[];
+}
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-about',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  data: DataItem = { count: 0, next:'', previous:'', results: [] };
+  chart: Chart;
+  isLoading = false;
+  
+  constructor(private http: HttpClient) {
+    this.chart = {} as Chart;
+   }
 
-}
+  ngOnInit() {
+   
+  }
+
+  fetchData(url: string) {
+    this.isLoading = true;
+    const headers = new HttpHeaders({ 
+      Authorization: 'Basic ' + btoa('sk_live_tpaWQddjrHK2gS5nk38x4oAr') 
+    });
+   
+    this.http.get<DataItem>(url, { headers })
+      .subscribe((res: DataItem) => {
+        this.data.count = res.count;
+        this.data.results = res.results;
+        this.data.next = res.next;
+        this.data.previous = res.previous;
+
+        this.createChart();
+        
+        this.isLoading = false;
+      });
+   
+  }
+
+  loadNext() {
+    if (this.data.next) {
+      this.fetchData(this.data.next);
+    }
+  }
+
+  loadPrevious() {
+    if (this.data.previous) {
+      this.fetchData(this.data.previous);
+    }
+  }
+
+  createChart() {
+    // If this.chart is an instance of Chart, destroy the existing chart
+    if (this.chart instanceof Chart) {
+      this.chart.destroy();
+    }
+  
+    const ctx = document.getElementById('myChart') as HTMLCanvasElement;
+    this.chart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: this.data.results.map(item => item.interval_start),
+        datasets: [{
+          label: 'Consumption',
+          data: this.data.results.map(item => item.consumption),
+          backgroundColor: 'rgba(75, 192, 192, 0.2)',
+          borderColor: 'rgba(75, 192, 192, 1)',
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        },
+        plugins: {
+          tooltip: {
+            enabled: true,
+            callbacks: {
+              label: function(context) {
+                return 'Consumption: ' + context.parsed.y;
+              }
+            }
+          }
+        }
+      }
+    });
+  }}
+
